c3b2796ad832534f95a921b1c27b4f012097efa6
T.Tian <lovaulonzezoeta@gmail.com>
2020-05-22 22:47:29 +0200

    Add example for mapping

---
+++ examples/ex5_mapping.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+
+###########################################################
+# The example shows how to get mapping  data              #
+# The peak ratio at 1315 cm^-1 and 1380 cm^-1 are plotted #
+# Details see Small 14, 1804006 (2018).                   # 
+###########################################################
+
+import numpy as np
+from renishawWiRE import WDFReader
+from pathlib import Path
+try:
+    import matplotlib.pyplot as plt
+    plot = True
+except ImportError:
+    plot = False
+
+
+def peak_in_range(spectra, wn, range, method="max", **params):
+    """Find the max intensity of peak within range
+       method can be max, min, or mean
+    """
+    cond = np.where((wn >= range[0]) & (wn <= range[1]))[0]
+    spectra_cut = spectra[:, :, cond]
+    return getattr(np, method)(spectra_cut, axis=2, **params)
+
+
+def main():
+    curdir = Path(__file__).parent.resolve()
+    filename = curdir / "spectra_files" / "mapping.wdf"
+    reader = WDFReader(filename)
+    assert reader.measurement_type == 3
+    wn = reader.xdata
+    spectra = reader.spectra
+    print("The size of mapping is {0:d} * {1:d}".
+          format(reader.spectra_w,
+                 reader.spectra_h))
+    print(wn.shape, spectra.shape)
+    x = reader.xpos
+    y = reader.ypos
+    w = reader.spectra_w
+    h = reader.spectra_h
+    spectra = spectra.reshape((h, w, len(wn)))
+    # w and h are the measure in xy coordinates
+    xx = x.reshape((h, w))
+    yy = y.reshape((h, w))
+    # Level the spectra
+    spectra = spectra - np.min(spectra, axis=2, keepdims=True)
+    peaks_a = peak_in_range(spectra, wn, [1295, 1340])
+    peaks_b = peak_in_range(spectra, wn, [1350, 1400])
+
+    ratio = peaks_a / peaks_b
+    ratio_fl = ratio.flatten()
+
+    if plot is True:
+        plt.figure(figsize=(10, 5))
+
+        # Left plot histogram of Peak A/B ratio
+        plt.subplot(121)
+        plt.hist(ratio_fl, bins=50, range=(0.1, 2))
+        plt.xlabel("Ratio peak A / peak B")
+        plt.ylabel("Counts")
+
+        # Right plot histogram of Peak A/B mapping
+        plt.subplot(122)
+        plt.imshow(ratio, interpolation="bicubic",
+                   extent=[0, xx.max() - xx.min(),
+                           yy.max() - yy.min(), 0],
+                   vmin=0.5, vmax=1.5)
+        plt.xlabel("x [μm]")
+        plt.ylabel("y [μm]")
+        cb = plt.colorbar()
+        cb.ax.set_title("Ratio")
+        plt.tight_layout()
+        plt.show(block=False)
+        plt.pause(5)
+        plt.close()
+    else:
+        pass
+    return
+
+
+if __name__ == "__main__":
+    main()
