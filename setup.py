0cdbcb3957c3885aea73c567e8b96ee2f96763cc
T.Tian <tian.tian@chem.ethz.ch>
2020-05-27 23:55:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 # from setuptools.command.install import install
 
 NAME = "renishawWiRE"
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 DESCRIPTION = open("README.md", encoding="utf-8").read()
 
 
