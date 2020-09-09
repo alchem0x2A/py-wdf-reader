f3684ae700ae0742a4395b17616f0f78630aa6cb
T.Tian <tian.tian@chem.ethz.ch>
2020-09-09 11:11:50 +0200

    To ver 0.1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 # from setuptools.command.install import install
 
 NAME = "renishawWiRE"
-VERSION = "0.1.9"
+VERSION = "0.1.10"
 DESCRIPTION = open("README.md", encoding="utf-8").read()
 
 
