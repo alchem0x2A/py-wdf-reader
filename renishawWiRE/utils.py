1b963f82cc89a3ac6ad3d023a808ec663c4f29a2
T.Tian <tian.tian@chem.ethz.ch>
2020-10-13 00:19:33 +0200

    Allow possible Nan wavenumber

--- renishawWiRE/utils.py
+++ renishawWiRE/utils.py
@@ -1,10 +1,14 @@
 # Some tiny functions for converting things
+from numpy import nan
 
 
 def convert_wl(wn):
     """Convert wavenumber (cm^-1) to nm
     """
-    wl = 1 / (wn * 1e2) / 1e-9
+    try:
+        wl = 1 / (wn * 1e2) / 1e-9
+    except ZeroDivisionError:
+        wl = nan
     return wl
 
 
@@ -12,4 +16,3 @@ def convert_attr_name(s):
     """Convert all underline in string name to space and capitalize
     """
     return " ".join(map(str.capitalize, s.strip().split("_")))
-    
