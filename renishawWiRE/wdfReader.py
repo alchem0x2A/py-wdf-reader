d6f1fd339dd7759b5cc93b350927a6fbdf483aac
T.Tian <alchem0x2a@gmail.com>
2021-07-24 17:30:39 -0400

    minor changes to comments in origin_list_header

--- renishawWiRE/wdfReader.py
+++ renishawWiRE/wdfReader.py
@@ -330,8 +330,10 @@ class WDFReader(object):
             # array = numpy.empty(self.count)
             
             # Time appears to be recorded as int64 in 100 nanosecond intervals
+            # Possibly using the .NET DateTime epoch
             # Reference does not appear to be  Unix Epoch time  
             # Set time[0] = 0 until timestamp reference can be determined
+            # Resulting array will have unit of `FileTime` in seconds
             if self.origin_list_header[i][1] == DataType.Time:             
                 array = numpy.array([self.__read_type("int64")
                                  for i in range(self.count)]) / 1e7
