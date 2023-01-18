42127f7e6532c86cabf01dc191ad766d644e6b49
T.Tian <alchem0x2a@gmail.com>
2023-01-18 16:59:44 +0000

    update setup.py classifiers

--- setup.py
+++ setup.py
@@ -58,9 +58,12 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     # scripts=[
     #   "bin/wdf-export",
